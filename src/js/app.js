d2f86c82d36b03694cf6cecb90a5a662e8effc9c
Andrey Kirichenko <andrey.kirichenko999@gmail.com>
2019-05-08 15:33:39 +0300

    updated packages

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,14 @@
 import '../scss/app.scss';
 import '../pug/index.pug'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+
+const App = (props) => {
+  return (<div>Apppp!</div>);
+};
+
+ReactDOM.render(<App/>, document.getElementById('root'));
